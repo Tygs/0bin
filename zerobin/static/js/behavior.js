a045de73d11f14700dedfcab8efa57abc9017eb4
sam <lesametlemax@gmail.com>
2012-05-24 14:41:26 +0200

    Going back to a very simple 'send by email' link since the previous attempt didn't work as expected

--- zerobin/static/js/behavior.js
+++ zerobin/static/js/behavior.js
@@ -739,15 +739,7 @@ $("#force-coloration").live("click", function(e) {
 
 /* Send the paste by email */
 var emailLink = 'mailto:friend@example.com?body=' + window.location;
-$('#email-link').attr('href', emailLink).bind('click', function(e){
-  $.get(emailLink).error(function(){
-    zerobin.message('error',
-                    ("You probably don't have one configured. Try " +
-                      "<a href='https://www.mozilla.org/en-US/thunderbird/'>Thunderbird</a> or " +
-                      "<a href='http://toolbar.google.com/gmail-helper/'>Gmail Notifier</a>" ),
-                    'Cannot start your email client', true);
-  });
-});
+$('#email-link').attr('href', emailLink);
 
 
 
